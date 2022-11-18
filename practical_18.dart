3000d241abdd209dc5d3561e4924b53535921ce7
tanmayvaij <tanmayvaij22@gmail.com>
2022-11-18 12:27:39 +0530

    refactored q18

--- practical_18.dart
+++ practical_18.dart
@@ -1,7 +1,7 @@
 // Capitalize the first character of a string
 
 String capitalize(String string) {
-  return string.substring(0, 1).toUpperCase() + string.substring(1, string.length);
+  return string[0].toUpperCase() + string.substring(1);
 }
 
 void main() {
